b358b821d800203eb2dc124c1f3da82af0642dcf
Sugar <403633318@qq.com>
2020-09-09 18:32:08 +0800

    change

--- content_script.js
+++ content_script.js
@@ -1,24 +1,6 @@
 
 window.addEventListener('click', evt => {
-	if (!evt.isTrusted) return;
-	let target = evt.target;
-	while (target && target.tagName.toLowerCase() !== 'a') {
-		target = target.parentElement;
-	}
-	if (target) {
-		// check for baseVal of svg a tag's href-SVGAnimatedString
-		const url = target instanceof SVGAElement ? target.href.baseVal : target.href;
-		console.log(url)
-		if (url.startsWith('file://')) {
-			evt.preventDefault();
-			try {
-				chrome.runtime.sendMessage({
-					method: 'openLocalFile',
-					localFileUrl: url,
-				});
-			} catch (e) {}
-		}
-	}
+	alert("hello,you are my sunshine~")
 }, {
 	capture: true,
 });
